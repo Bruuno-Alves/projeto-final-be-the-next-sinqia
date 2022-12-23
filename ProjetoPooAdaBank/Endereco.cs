69ef297a08ca43524b16dbdef75950d5631afd3e
Bruuno-Alves <bruno.a.santos@live.com>
2022-12-23 10:09:58 -0300

    First commit

---
+++ ProjetoPooAdaBank/Endereco.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ProjetoPooAdaBank
+{
+    public class Endereco
+    {
+        public string Rua { get; set; }
+        public int Numero { get; set; }
+        public string Bairro { get; set; }
+        public string Cidade { get; set; }
+        public string Estado { get; set; }
+
+        public Endereco(string rua, int numero, string bairro, string cidade, string estado)
+        {
+            Rua = rua;
+            Numero = numero;
+            Bairro = bairro;
+            Cidade = cidade;
+            Estado = estado;    
+        }
+
+        public override string ToString()
+        {
+            return $"Rua {Rua}, nº {Numero} - {Bairro} - {Cidade} - {Estado}";
+        }
+    }
+}
---
+++ ProjetoPooAdaBank/Transacao.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ProjetoPooAdaBank
+{
+    public class Transacao
+    {
+        string Tipo { get; set; }
+        public double Valor { get; set; }
+
+        public Transacao(string tipo, double valor)
+        {
+            Tipo = tipo;
+            Valor = valor;
+        }
+
+        public override string ToString()
+        {
+            return $"Transação: {Tipo} - Valor: {Valor}";
+        }
+    }
+
+
+}
