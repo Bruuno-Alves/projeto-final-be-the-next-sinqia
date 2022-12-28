f4e774a165015f548afbd5cf1fddf3859b384d89
JuniorLobato <elenilson_lobato@hotmail.com>
2022-12-28 13:30:17 -0300

    Conta Poupanca

---
+++ ProjetoPooAdaBank/ContaPoupanca1/SaldoMinimo.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Cryptography;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ProjetoPooAdaBank.ContaPoupanca1 {
+    internal class SaldoMinimo {
+
+        //propriedade
+        public double saldoMinimoPoupanca { get; private set; }
+
+        //construtor
+        public SaldoMinimo(double valorSaldoMinimo) 
+            {
+
+            //condição para criar a poupança com um limite mínimo de 50 reais
+            if(valorSaldoMinimo >= 50.0) 
+                {
+                saldoMinimoPoupanca = valorSaldoMinimo;
+            } else {
+                Console.WriteLine("Saldo mínimo inválido para criar a conta poupança!");
+            }
+            
+
+        }
+    }
+}
--- ProjetoPooAdaBank/Program.cs
+++ ProjetoPooAdaBank/Program.cs
@@ -1,5 +1,5 @@
 ﻿using ProjetoPooAdaBank.ContaSalario3;
-
+using ProjetoPooAdaBank.ContaPoupanca1;
 namespace ProjetoPooAdaBank
 {
     public class Program
@@ -9,9 +9,16 @@ namespace ProjetoPooAdaBank
             Endereco endereco1 = new Endereco("Rua Jose Batista", 44, "Centro", "Itabaiana", "SE");
             Cliente cliente1 = new Cliente("Jonas Augusto", "000.000.000-00", endereco1);
             ContaSalario cs = new ContaSalario(28, 123, cliente1, "11.111.111/0001-11", 2500);
+            ContaPoupanca cp = new ContaPoupanca(13, 131313, cliente1, 50);
 
             Console.WriteLine(cs.Saldo);
             cs.DepositarSalario(2500, "11.111.111/0001-11");
+            
+
+            cp.DepositarPoupanca(50);
+            cp.CalcularValorTarifaManutencao();
+            Console.WriteLine(cp.Saldo);
+
 
             Console.WriteLine(Conta.ContasAbertas);
         }
