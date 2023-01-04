9b85ce74801bb8dc6fe5288df33c9d8c7eba3059
Richard Cabral <richardcabraloliveira@gmail.com>
2023-01-04 12:06:30 -0300

    "Limpando a sujeira do commit anterior"

--- ProjetoPooAdaBank/Contas/ContaInvestimento.cs
+++ ProjetoPooAdaBank/Contas/ContaInvestimento.cs
@@ -1,61 +1,56 @@
 ﻿using ProjetoPooAdaBank.Clientes;
 using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
-namespace ProjetoPooAdaBank.Contas
+namespace ProjetoPooAdaBank.Investimento
 {
-    public class ContaInvestimento : Conta
-    {
-        public string PerfilInvestidor { get; private set; }
+	public class ContaInvestimento : Conta
+	{
+		public string PerfilInvestidor { get; private set; }
 
-        public ContaInvestimento(int numeroAgencia, int numeroConta, Cliente titular) : base(numeroAgencia, numeroConta, titular)
-        {
-            this.TipoConta = 3;
-            PerfilInvestidor = AvaliaPerfil();   
+        public ContaInvestimento(int numeroAgencia, int numeroConta, Cliente titular) : base(numeroAgencia,numeroConta,titular)
+		{
+			 PerfilInvestidor = AvaliaPerfil();
         }
 
         public void Investir()
         {
             int opcao;
             double valor;
-
+            
             string infoExtrato;
             do
             {
                 Console.WriteLine("Que tipo de investimento deseja realizar?\n" +
                     "[1] - Renda Fixa\n[2] - Ações");
-
+               
                 int.TryParse(Console.ReadLine(), out opcao);
-            } while (opcao != 1 && opcao != 2);
-            if (opcao == 1)
+            }while(opcao != 1 || opcao != 2);
+            if(opcao == 1)
             {
                 infoExtrato = "Investimento - Renda Fixa ";
-                valor = RendaFixa();
+                valor = RendaFixa(); 
             }
             else
             {
                 infoExtrato = "Investimento - Ações ";
                 valor = Acoes();
             }
-            if (valor == 0)
+            if(valor == 0)
             {
                 Console.WriteLine("Operação cancelada.");
             }
             else
             {
                 Saldo -= valor;
-                Extrato.Add(new Transacao(infoExtrato, valor, Saldo));
+                Extrato.Add(new Transacao(infoExtrato, valor));
             }
         }
         private double Acoes()
         {
-            int opcao, quant, loop;
+            int opcao,quant,loop;
             double valorAcao = 0, valorTotal;
 
-            if (Saldo < 50)
+            if(Saldo < 50)
             {
                 Console.WriteLine("É necessário ao menos 50R$ em conta para realizar um investimento.");
                 return 0;
@@ -73,7 +68,7 @@ namespace ProjetoPooAdaBank.Contas
                     "[5] - AdaBank - 48,32R$");
                     Console.WriteLine("Qual das ações listadas deseja comprar?");
                     int.TryParse(Console.ReadLine(), out opcao);
-                } while (opcao <= 0 && opcao > 5);
+                } while (opcao <= 0 || opcao > 5);
 
                 switch (opcao)
                 {
@@ -99,16 +94,16 @@ namespace ProjetoPooAdaBank.Contas
                 {
                     Console.WriteLine("Quantas ações deseja comprar?");
                     int.TryParse(Console.ReadLine(), out quant);
-
+                    
                     valorTotal = quant * valorAcao;
                     Console.WriteLine($"O total que será pago nas ações será de: {valorTotal}R$");
-                    if (valorTotal > Saldo)
+                    if(valorTotal > Saldo)
                     {
-                        Console.WriteLine($"O valor da(s) ação(ões)({valorTotal}R$) ultrapassa seu saldo({Saldo}R$)," +
+                        Console.WriteLine($"O valor da(s) ação(ões)({valorTotal}R$) ultrassa seu saldo({Saldo}R$)," +
                             $" tente uma quantia menor de ações");
 
                     }
-                } while (quant <= 0 || valorTotal > Saldo);
+                } while(quant <=0 ||  valorTotal > Saldo);
 
                 Console.WriteLine($"O valor pago nas ações será de {valorTotal}R$, seu novo saldo será de {Saldo - valorTotal}\n" +
                     $"[1] - Concluir, [2] - Reiniciar , [3] - Cancelar");
@@ -120,20 +115,20 @@ namespace ProjetoPooAdaBank.Contas
                     break;
                 }
 
-            } while (loop != 1);
+            } while (loop !=1);
 
             return valorTotal;
         }
         private double RendaFixa()
         {
-            double valor, previaInvestimento = 0;
-            int opcao, loop;
+            double valor, previaInvestimento=0;
+            int opcao,loop;
             do
             {
                 Console.Clear();
                 Console.WriteLine("Informe quanto deseja investir, " +
                     "após isso escolha entre as opções e veja uma prévia do valor (sem correção)");
-
+                
                 do
                 {
                     double.TryParse(Console.ReadLine(), out valor);
@@ -143,7 +138,7 @@ namespace ProjetoPooAdaBank.Contas
                         Console.WriteLine("Valor negativo ou superior ao seu saldo, tente novamente.");
                     }
                 } while (valor <= 0 || valor > Saldo);
-
+                
                 do
                 {
                     Console.WriteLine("As opções de investimento disponíveis são:\n" +
@@ -173,15 +168,15 @@ namespace ProjetoPooAdaBank.Contas
                 }
 
                 Console.WriteLine($"A previsão sem reajustes é de que o investimento no valor de {valor}R$, após o tempo estipulado será de {previaInvestimento}." +
-                    $"Ao confirmar o investimento seu novo saldo será {Saldo - valor}R$");
+                    $"Ao confirmar o investimento seu novo saldo será {Saldo-valor}R$");
                 Console.WriteLine("Deseja realizar o investimento? \n[1] - SIM \n[2] - Nova Simulação\n[3] - Cancelar");
-                int.TryParse(Console.ReadLine(), out loop);
-                if (loop == 3)
+                int.TryParse(Console.ReadLine(),out loop);
+                if(loop == 3)
                 {
                     valor = 0;
                     break;
                 }
-            } while (loop != 1);
+            } while(loop != 1);
 
             return valor;
 
@@ -207,21 +202,21 @@ namespace ProjetoPooAdaBank.Contas
                                   "de 10% alguns dias após seu investimento?;");
                 Console.WriteLine("[1] = Venderia\n[2] = Aguardaria \n[3] = Compraria mais");
                 int.TryParse(Console.ReadLine(), out r2);
-            } while (r2 < 1 || r2 > 3);
+            } while ( r2 < 1 || r2 > 3);
             Console.Clear();
             do
             {
                 Console.WriteLine("3ª - Por quanto tempo pretende deixar seus investimentos?");
                 Console.WriteLine("[1] = Até 2 anos\n[2] = Entre 3 e 5 anos \n[3] = Acima de 5 anos");
-                int.TryParse(Console.ReadLine(), out r3);
-            } while (r3 < 1 || r3 > 3);
+                 int.TryParse(Console.ReadLine(), out r3);
+            } while ( r3 < 1 || r3 > 3);
             Console.Clear();
             do
             {
                 Console.WriteLine("4ª - Quanto da sua renda pretende investir?");
                 Console.WriteLine("[1] = Até 10%\n[2] = Entre 11% e 20%\n[3] = Acima de 20%");
                 validaResposta = int.TryParse(Console.ReadLine(), out r4);
-            } while (r4 < 1 || r4 > 3);
+            } while ( r4 < 1 || r4 > 3);
             Console.Clear();
             do
             {
@@ -230,9 +225,9 @@ namespace ProjetoPooAdaBank.Contas
                     "[2] = Proteger meu patrimônio e lucrar a médio Prazo \n" +
                     "[3] = Lucrar a longo prazo");
                 validaResposta = int.TryParse(Console.ReadLine(), out r5);
-            } while (r5 < 1 || r5 > 3);
+            } while ( r5 < 1 || r5 > 3);
             Console.Clear();
-            int media = (r1 * 3 + r2 + r3 + r4 + r5) / 5;
+            int media = (r1 * 3 + r2 + r3 + r4 + r5)/5;
             if (media == 0)
             {
                 media = 1;
